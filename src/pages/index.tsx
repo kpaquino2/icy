34571f4fb4b33784dcbece7e555b5ac0e0d3108f
Kyle Aquino <kpaquino2@up.edu.ph>
2023-02-06 11:00:21 +0800

    refactor: Remove unused imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
 import { type NextPage } from "next";
-import Head from "next/head";
-import Link from "next/link";
 import Layout from "../components/Layout/Layout";
 
 const Home: NextPage = () => {
