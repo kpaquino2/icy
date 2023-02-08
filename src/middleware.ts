249325843d73615ca094bf98b562d895f2e76091
Kyle Aquino <kpaquino2@up.edu.ph>
2023-02-08 11:14:32 +0800

    feat: Middleware to check if auth in sign up page

---
+++ src/middleware.ts
@@ -0,0 +1,10 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  if (request.cookies.get("next-auth.session-token"))
+    return NextResponse.redirect(new URL("/", request.url));
+}
+
+export const config = {
+  matcher: "/sign-up",
+};
