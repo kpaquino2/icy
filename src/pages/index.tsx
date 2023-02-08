29a117da00eeae4a2e440d4d5720487f439ac57b
Kyle Aquino <kpaquino2@up.edu.ph>
2023-02-08 11:15:48 +0800

    feat: Show auth status in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
 import { type NextPage } from "next";
 import { useSession } from "next-auth/react";
+import Link from "next/link";
 import { X } from "phosphor-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Layout from "../components/Layout/Layout";
 
 const Home: NextPage = () => {
   const { data: session } = useSession();
   const [showNotice, setShowNotice] = useState(true);
+
+  useEffect(() => {
+    if (session) setShowNotice(false);
+  }, [session]);
 
   return (
     <>
@@ -15,12 +20,19 @@ const Home: NextPage = () => {
         description="list of all curricula made by the user"
         crumbs="curricula"
       >
-        {!session && showNotice && (
-          <div className="flex justify-between bg-teal-600 p-1 text-sm text-zinc-100 dark:bg-teal-400 dark:text-zinc-900">
-            create an account to access your curricula from other devices
-            <button type="button" onClick={() => setShowNotice(false)}>
+        {showNotice && (
+          <div className="flex gap-2 bg-teal-600 p-1 text-sm text-zinc-100 dark:bg-teal-400 dark:text-zinc-900">
+            <button
+              type="button"
+              onClick={() => setShowNotice(false)}
+              className="place-self-end"
+            >
               <X size={20} />
             </button>
+            create an account to access your curricula from other devices.
+            <Link href="sign-up" className="underline">
+              click here to sign up
+            </Link>
           </div>
         )}
         <div className="grid grid-cols-1 p-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
