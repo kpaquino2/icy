238935d2062101d9d835de8d5a1c4d54d8a46d90
Kyle Aquino <kpaquino2@up.edu.ph>
2023-02-15 18:35:46 +0800

    feat: Course details and delete course function

--- src/components/Curriculum/Course.tsx
+++ src/components/Curriculum/Course.tsx
@@ -1,11 +1,47 @@
 import { Popover, Transition } from "@headlessui/react";
 import { Course } from "@prisma/client";
+import { PencilSimpleLine, TrashSimple } from "phosphor-react";
+import { api } from "../../utils/api";
 
 interface CourseProps {
   course: Course;
 }
 
 const Course = ({ course }: CourseProps) => {
+  const tctx = api.useContext();
+  const { mutate: deleteCourseMutation } = api.course.deleteCourse.useMutation({
+    onMutate: async (input) => {
+      await tctx.curriculum.getCurriculum.cancel();
+      const prev = tctx.curriculum.getCurriculum.getData();
+      tctx.curriculum.getCurriculum.setData(undefined, (old) => {
+        if (!old) return;
+        const newsems = old.sems.map((s) =>
+          s.id === course.semesterId
+            ? {
+                ...s,
+                courses: s.courses.filter((c) => c.id !== input.id),
+              }
+            : s
+        );
+        return {
+          ...old,
+          sems: newsems,
+        };
+      });
+      return { prev };
+    },
+    onError: (err, input, ctx) => {
+      tctx.curriculum.getCurriculum.setData(undefined, ctx?.prev);
+    },
+    onSettled: async () => {
+      await tctx.curriculum.getCurriculum.refetch();
+    },
+  });
+
+  const handleDelete = () => {
+    deleteCourseMutation({ id: course.id });
+  };
+
   return (
     <Popover>
       <Popover.Button
@@ -22,13 +58,29 @@ const Course = ({ course }: CourseProps) => {
         leaveFrom="opacity-100"
         leaveTo="opacity-0"
       >
-        <Popover.Panel className="absolute z-10 ml-1 max-h-48 w-48 -translate-y-10 translate-x-44 overflow-auto rounded border-2 border-zinc-200 bg-zinc-100 p-2 dark:border-zinc-800 dark:bg-zinc-900">
-          <div className="grid grid-cols-3">
-            <div className="col-span-2">{course.code}</div>
-            <div>{course.courseUnits} units</div>
-            <div className="col-span-3">{course.title}</div>
-            <div className="col-span-3">{course.description}</div>
+        <Popover.Panel className="absolute z-10 ml-1 w-64 -translate-y-10 translate-x-44 overflow-auto rounded border-2 border-zinc-200 bg-zinc-100 p-3 dark:border-zinc-800 dark:bg-zinc-900">
+          <div className="flex justify-end gap-3">
+            <button className="rounded text-zinc-400 hover:text-teal-600 hover:dark:text-teal-400">
+              <PencilSimpleLine size={16} weight="bold" />
+            </button>
+            <button
+              onClick={handleDelete}
+              className="rounded text-zinc-400 hover:text-teal-600 hover:dark:text-teal-400"
+            >
+              <TrashSimple size={16} weight="bold" />
+            </button>
           </div>
+          <div className="flex items-center justify-between">
+            <div className="text-xl leading-none">{course.code}</div>
+            <div>
+              {course.courseUnits} unit{course.courseUnits !== 1 && "s"}
+            </div>
+          </div>
+
+          <div className="text-sm leading-none text-zinc-600 dark:text-zinc-400">
+            {course.title}
+          </div>
+          <div className="mt-1 leading-tight">{course.description}</div>
         </Popover.Panel>
       </Transition>
     </Popover>
