1061e31d69a33b74d4ad0f3b60d3c5a40a62821a
Kyle Aquino <kpaquino2@up.edu.ph>
2023-02-25 19:24:55 +0800

    feat: Course details positioning with floating ui

--- src/components/Curriculum/Course.tsx
+++ src/components/Curriculum/Course.tsx
@@ -1,8 +1,9 @@
+import { autoPlacement, offset, size, useFloating } from "@floating-ui/react";
 import { Popover, Transition } from "@headlessui/react";
 import { Draggable } from "@hello-pangea/dnd";
 import { Course } from "@prisma/client";
 import { PencilSimpleLine, TrashSimple } from "phosphor-react";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { api } from "../../utils/api";
 import { useCurriculumStore } from "../../utils/stores/curriculumStore";
 import EditCourseModal from "./EditCourseModal";
@@ -47,6 +48,34 @@ const Course = ({ course, index }: CourseProps) => {
     deleteCourseMutation({ id: course.id, semesterId: course.semesterId });
   };
 
+  const {
+    floating,
+    reference,
+    strategy: position,
+    x,
+    y,
+  } = useFloating({
+    middleware: [
+      offset(6),
+      autoPlacement({
+        allowedPlacements: [
+          "right-start",
+          "left-start",
+          "right-end",
+          "left-end",
+        ],
+      }),
+      size({
+        apply: ({ elements }) => {
+          Object.assign(elements.floating.style, {
+            width: "16rem",
+          });
+        },
+      }),
+    ],
+  });
+  const hackRef = useRef<HTMLDivElement>(null);
+
   return (
     <>
       <EditCourseModal
@@ -56,7 +85,7 @@ const Course = ({ course, index }: CourseProps) => {
         title="edit course"
       />
       <Draggable draggableId={course.id} index={index}>
-        {(provided) => (
+        {(provided, snapshot) => (
           <Popover
             className="my-2 w-3/4"
             ref={provided.innerRef}
@@ -65,51 +94,73 @@ const Course = ({ course, index }: CourseProps) => {
           >
             <Popover.Button
               as="div"
-              // type="div"
+              ref={reference}
               className="flex w-full justify-center rounded bg-teal-600 py-2 text-zinc-100 hover:brightness-125 dark:bg-teal-400 dark:text-zinc-900"
             >
               {course.code}
             </Popover.Button>
-            <Transition
-              enter="transition duration-100 ease-out"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="transition duration-75 ease-out"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
+            <div
+              ref={hackRef}
+              style={{
+                position,
+                top: y ?? 0,
+                left: x ?? 0,
+              }}
             >
-              <Popover.Panel className="absolute z-10 ml-2 w-64 -translate-y-10 translate-x-32 overflow-auto rounded border-2 border-zinc-200 bg-zinc-100 p-3 dark:border-zinc-800 dark:bg-zinc-900">
-                <div className="flex justify-end gap-3">
-                  <button
-                    onClick={() => setEditCourseOpen(true)}
-                    className="rounded text-zinc-400 hover:text-teal-600 hover:dark:text-teal-400"
-                  >
-                    <PencilSimpleLine size={16} weight="bold" />
-                  </button>
-                  <button
-                    onClick={handleDelete}
-                    className="rounded text-zinc-400 hover:text-teal-600 hover:dark:text-teal-400"
-                  >
-                    <TrashSimple size={16} weight="bold" />
-                  </button>
-                </div>
-                <div className="flex items-center justify-between">
-                  <div className="text-xl leading-none">{course.code}</div>
-                  <div>
-                    {course.units} unit{course.units !== 1 && "s"}
-                  </div>
-                </div>
+              <Transition
+                enter="transition duration-100 ease-out"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="transition duration-75 ease-out"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+                beforeEnter={() => floating(hackRef.current)}
+                afterLeave={() => floating(null)}
+              >
+                <Popover.Panel className="rounded border-2 border-zinc-200 bg-zinc-100 p-3 dark:border-zinc-800 dark:bg-zinc-900">
+                  {({ close }) => {
+                    if (snapshot.isDragging) close();
+                    return (
+                      <>
+                        <div className="flex justify-end gap-3">
+                          <button
+                            onClick={() => setEditCourseOpen(true)}
+                            className="rounded text-zinc-400 hover:text-teal-600 hover:dark:text-teal-400"
+                          >
+                            <PencilSimpleLine size={16} weight="bold" />
+                          </button>
+                          <button
+                            onClick={handleDelete}
+                            className="rounded text-zinc-400 hover:text-teal-600 hover:dark:text-teal-400"
+                          >
+                            <TrashSimple size={16} weight="bold" />
+                          </button>
+                        </div>
+                        <div className="flex items-center justify-between">
+                          <div className="text-xl leading-none">
+                            {course.code}
+                          </div>
+                          <div>
+                            {course.units} unit{course.units !== 1 && "s"}
+                          </div>
+                        </div>
 
-                <div className="text-sm leading-none text-zinc-600 dark:text-zinc-400">
-                  {course.title || <p className="italic">no course title</p>}
-                </div>
-                <div className="mt-1 leading-tight">
-                  {course.description || (
-                    <p className="italic">no course description</p>
-                  )}
-                </div>
-              </Popover.Panel>
-            </Transition>
+                        <div className="text-sm leading-none text-zinc-600 dark:text-zinc-400">
+                          {course.title || (
+                            <p className="italic">no course title</p>
+                          )}
+                        </div>
+                        <div className="mt-1 leading-tight">
+                          {course.description || (
+                            <p className="italic">no course description</p>
+                          )}
+                        </div>
+                      </>
+                    );
+                  }}
+                </Popover.Panel>
+              </Transition>
+            </div>
           </Popover>
         )}
       </Draggable>
