5c339cf111f39a5a04c5e24c1c4869e972255a51
Kyle Aquino <kpaquino2@up.edu.ph>
2023-03-05 13:27:10 +0800

    style: Sem border as background

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,13 @@ module.exports = {
       animation: {
         load: "spin 2s ease-in-out infinite alternate-reverse",
       },
+      backgroundImage: {
+        borderright:
+          "repeating-linear-gradient(270deg, var(--tw-gradient-stops) 0 2px, transparent 0 100%)",
+      },
+      backgroundSize: {
+        col: "192px",
+      },
     },
   },
   plugins: [],
