8cf56c95018d1d952c9bd07627f6c548e8c7b2c3
Kyle Aquino <kpaquino2@up.edu.ph>
2023-03-16 05:30:17 +0800

    feat: Thicker focusable path for connections

--- src/components/Curriculum/Connection/Line.tsx
+++ src/components/Curriculum/Connection/Line.tsx
@@ -26,24 +26,21 @@ const Line = ({ pre, post, focused, focus, blur }: LineProps) => {
   const pathRef = useRef<SVGPathElement>(null);
   const dist = pathRef.current?.getTotalLength();
   return (
-    <g
-      className={
-        (focused
-          ? "stroke-teal-600 opacity-100 dark:stroke-teal-400"
-          : "stroke-zinc-500 opacity-50 ") +
-        " cursor-pointer fill-none stroke-2 transition-all focus:outline-none"
-      }
-      tabIndex={1}
-      onFocus={focus}
-      onBlur={blur}
-    >
+    <g>
       <path
+        className="stroke-zinc-500 stroke-2 opacity-50"
         d={`M ${start[0]} ${start[1] - 5.66} L ${start[0] + 5.66} ${start[1]}`}
       />
       <path
+        className="stroke-zinc-500 stroke-2 opacity-50"
         d={`M ${start[0]} ${start[1] + 5.66} L ${start[0] + 5.66} ${start[1]}`}
       />
       <path
+        className={
+          (focused
+            ? "stroke-teal-600 opacity-100 dark:stroke-teal-400"
+            : "stroke-zinc-500 opacity-50 ") + " fill-none stroke-2"
+        }
         ref={pathRef}
         d={`M ${start[0] + 4} ${start[1]}
             L ${start[0] + (end[0] - start[0]) / 4} ${start[1]}
@@ -51,8 +48,25 @@ const Line = ({ pre, post, focused, focus, blur }: LineProps) => {
             L ${end[0]} ${end[1]}
           `}
       />
-      <path d={`M ${end[0] - 5.66} ${end[1] - 5.66} L ${end[0]} ${end[1]}`} />
-      <path d={`M ${end[0] - 5.66} ${end[1] + 5.66} L ${end[0]} ${end[1]}`} />
+      <path
+        className="cursor-pointer fill-none stroke-transparent stroke-[20px]"
+        d={`M ${start[0] + 4} ${start[1]}
+            L ${start[0] + (end[0] - start[0]) / 4} ${start[1]}
+            L ${end[0] - (end[0] - start[0]) / 4} ${end[1]}
+            L ${end[0]} ${end[1]}
+          `}
+        tabIndex={1}
+        onFocus={focus}
+        onBlur={blur}
+      />
+      <path
+        className="stroke-zinc-500 stroke-2 opacity-50"
+        d={`M ${end[0] - 5.66} ${end[1] - 5.66} L ${end[0]} ${end[1]}`}
+      />
+      <path
+        className="stroke-zinc-500 stroke-2 opacity-50"
+        d={`M ${end[0] - 5.66} ${end[1] + 5.66} L ${end[0]} ${end[1]}`}
+      />
       {focused && (
         <polygon
           points="0,-3 3,0 0,3 -3,0"
