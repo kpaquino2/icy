644939e1ce2ec1451ba2b16e6243799d0a07406d
Kyle Aquino <kpaquino2@up.edu.ph>
2023-03-17 21:45:51 +0800

    feat: Page for settings

---
+++ src/pages/settings.tsx
@@ -0,0 +1,14 @@
+import { type NextPage } from "next";
+import Layout from "../components/Layout/Layout";
+
+const Settings: NextPage = () => {
+  return (
+    <>
+      <Layout title="settings" description="customize your settings with east">
+        bleh
+      </Layout>
+    </>
+  );
+};
+
+export default Settings;
