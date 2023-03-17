cf3d0a61f7f4b3b171a00dd77ce1d7bef7e6866a
Kyle Aquino <kpaquino2@up.edu.ph>
2023-03-17 12:32:07 +0800

    feat: Improve loading state when creating curric

--- src/components/Curriculum/AddCurriculumModal.tsx
+++ src/components/Curriculum/AddCurriculumModal.tsx
@@ -75,7 +75,7 @@ const AddCurriculumModal = ({
         await tctx.curriculum.getCurriculum.refetch();
       },
     });
-  const { mutate: createNewCurriculumFromTemplateMutation, isLoading } =
+  const { mutateAsync: createNewCurriculumFromTemplateMutation } =
     api.curriculum.createCurriculumFromTemplate.useMutation({
       onError: (err, input) => {
         if (err.data?.code === "UNAUTHORIZED") {
@@ -139,18 +139,22 @@ const AddCurriculumModal = ({
   const onSubmit = async (data: Schema) => {
     const template = await getTemplateByCode({ code: data.template });
     if (!template) return;
-    createNewCurriculumFromTemplateMutation({
-      curriculum: {
-        ...template.curriculum,
-        id: createId(),
-      },
-    });
+    try {
+      await createNewCurriculumFromTemplateMutation({
+        curriculum: {
+          ...template.curriculum,
+          id: createId(),
+        },
+      });
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   return (
     <Modal
       isOpen={newCurricOpen}
-      close={() => setNewCurricOpen(false)}
+      close={() => setNewCurricOpen(isSubmitting)}
       width="w-[600px]"
       title={title}
     >
@@ -190,7 +194,7 @@ const AddCurriculumModal = ({
             <div className="relative p-0.5">
               <input
                 type="text"
-                disabled={isLoading}
+                disabled={isSubmitting}
                 className="peer w-full rounded border-2 border-zinc-200 bg-inherit px-3 py-1 pl-8 focus:border-teal-600 focus:outline-none focus:ring-1 focus:ring-teal-600 dark:border-zinc-800 focus:dark:border-teal-400 focus:dark:ring-teal-400"
                 onChange={(e) => setSearch(e.target.value.toLowerCase().trim())}
               />
@@ -202,13 +206,13 @@ const AddCurriculumModal = ({
             </div>
             <div
               className={
-                (isLoading ? "overflow-y-hidden" : "overflow-y-auto") +
+                (isSubmitting ? "overflow-y-hidden" : "overflow-y-auto") +
                 " relative overflow-y-auto rounded border-2 border-zinc-200 dark:border-zinc-800"
               }
             >
               <div
                 className={
-                  (isLoading ? "opacity-50" : "opacity-100") +
+                  (isSubmitting ? "opacity-50" : "opacity-100") +
                   " rounded border-r-2 border-zinc-200 dark:border-zinc-800"
                 }
               >
@@ -237,7 +241,7 @@ const AddCurriculumModal = ({
                             tabIndex={0}
                             key={index}
                             className={
-                              (isLoading
+                              (isSubmitting
                                 ? ""
                                 : "hover:bg-teal-600 hover:text-zinc-100 dark:hover:bg-teal-400 dark:hover:text-zinc-900") +
                               " flex justify-between border-zinc-200 px-4 py-1.5 last:border-b-2  peer-checked:text-teal-600 dark:border-zinc-800 dark:peer-checked:text-teal-400"
@@ -246,7 +250,7 @@ const AddCurriculumModal = ({
                             <input
                               type="radio"
                               className="peer hidden"
-                              disabled={isLoading}
+                              disabled={isSubmitting}
                               id={c}
                               value={c}
                               {...register("template")}
@@ -266,21 +270,48 @@ const AddCurriculumModal = ({
             <div className="text-end text-xs italic text-zinc-500">
               source: https://amis.uplb.edu.ph/curriculums-management/
             </div>
-            <div className="flex justify-end gap-3">
-              <button
-                type="button"
-                onClick={handleBack}
-                className="rounded border-2 border-teal-600 px-2 py-1 text-teal-600 transition hover:brightness-110 disabled:opacity-50 dark:border-teal-400 dark:text-teal-400"
-                disabled={isLoading}
-              >
-                back
-              </button>
-              <button
-                className="flex items-center gap-2 rounded bg-teal-600 px-2 py-1 text-zinc-100 transition enabled:hover:brightness-110 disabled:opacity-50 dark:bg-teal-400 dark:text-zinc-900"
-                disabled={isSubmitting || !isDirty || isLoading}
-              >
-                confirm
-              </button>
+            <div className="flex flex-row-reverse items-center justify-between">
+              <div className="flex gap-3">
+                <button
+                  type="button"
+                  onClick={handleBack}
+                  className="rounded border-2 border-teal-600 px-2 py-1 text-teal-600 transition hover:brightness-110 disabled:opacity-50 dark:border-teal-400 dark:text-teal-400"
+                  disabled={isSubmitting}
+                >
+                  back
+                </button>
+                <button
+                  className="flex items-center gap-2 rounded bg-teal-600 px-2 py-1 text-zinc-100 transition enabled:hover:brightness-110 disabled:opacity-50 dark:bg-teal-400 dark:text-zinc-900"
+                  disabled={isSubmitting || !isDirty}
+                >
+                  confirm
+                </button>
+              </div>
+              {isSubmitting && (
+                <svg
+                  width="32"
+                  height="32"
+                  viewBox="0 0 100 100"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="animate-load fill-teal-600 dark:fill-teal-400"
+                >
+                  <g clipPath="url(#clip0_0_1)">
+                    <path d="M73.9142 77.9142L56.4142 60.4142C55.1543 59.1543 53 60.0466 53 61.8284V77.6716C53 78.202 53.2107 78.7107 53.5858 79.0858L71.0858 96.5858C72.3457 97.8457 74.5 96.9534 74.5 95.1716V79.3284C74.5 78.798 74.2893 78.2893 73.9142 77.9142Z" />
+                    <path d="M22.0858 26.0858L39.5858 43.5858C40.8457 44.8457 39.9534 47 38.1716 47H22.3284C21.798 47 21.2893 46.7893 20.9142 46.4142L3.41422 28.9142C2.15429 27.6543 3.04662 25.5 4.82843 25.5L20.6716 25.5C21.202 25.5 21.7107 25.7107 22.0858 26.0858Z" />
+                    <path d="M79.0858 53.5858L96.5858 71.0858C97.8457 72.3457 96.9534 74.5 95.1716 74.5H79.3284C78.798 74.5 78.2893 74.2893 77.9142 73.9142L60.4142 56.4142C59.1543 55.1543 60.0466 53 61.8284 53H77.6716C78.202 53 78.7107 53.2107 79.0858 53.5858Z" />
+                    <path d="M79.0858 46.4142L96.5858 28.9142C97.8457 27.6543 96.9534 25.5 95.1716 25.5H79.3284C78.798 25.5 78.2893 25.7107 77.9142 26.0858L60.4142 43.5858C59.1543 44.8457 60.0466 47 61.8284 47H77.6716C78.202 47 78.7107 46.7893 79.0858 46.4142Z" />
+                    <path d="M22.0858 73.9142L39.5858 56.4142C40.8457 55.1543 39.9534 53 38.1716 53H22.3284C21.798 53 21.2893 53.2107 20.9142 53.5858L3.41422 71.0858C2.15429 72.3457 3.04662 74.5 4.82843 74.5H20.6716C21.202 74.5 21.7107 74.2893 22.0858 73.9142Z" />
+                    <path d="M26.0858 77.9142L43.5858 60.4142C44.8457 59.1543 47 60.0466 47 61.8284V77.6716C47 78.202 46.7893 78.7107 46.4142 79.0858L28.9142 96.5858C27.6543 97.8457 25.5 96.9534 25.5 95.1716V79.3284C25.5 78.798 25.7107 78.2893 26.0858 77.9142Z" />
+                    <path d="M53.5858 20.9142L71.0858 3.41421C72.3457 2.15428 74.5 3.04662 74.5 4.82843V20.6716C74.5 21.202 74.2893 21.7107 73.9142 22.0858L56.4142 39.5858C55.1543 40.8457 53 39.9534 53 38.1716V22.3284C53 21.798 53.2107 21.2893 53.5858 20.9142Z" />
+                    <path d="M46.4142 20.9142L28.9142 3.41421C27.6543 2.15428 25.5 3.04662 25.5 4.82843V20.6716C25.5 21.202 25.7107 21.7107 26.0858 22.0858L43.5858 39.5858C44.8457 40.8457 47 39.9534 47 38.1716V22.3284C47 21.798 46.7893 21.2893 46.4142 20.9142Z" />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_0_1">
+                      <rect width="100" height="100" fill="white" />
+                    </clipPath>
+                  </defs>
+                </svg>
+              )}
             </div>
           </form>
         </div>
