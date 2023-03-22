ac69ac05cc545941d6e463cea78557c5879f988b
Kyle Aquino <kpaquino2@up.edu.ph>
2023-03-22 13:42:30 +0800

    feat: Sign in with Google button

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -49,6 +49,45 @@ const SignUpPage = () => {
           </div>
           <button
             type="button"
+            className="mb-2 flex h-10 items-center rounded bg-zinc-50 px-3 text-zinc-900 shadow"
+            onClick={() => void handleSignIn("google")}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              viewBox="0 0 48 48"
+              width={24}
+            >
+              <defs>
+                <path
+                  id="a"
+                  d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"
+                />
+              </defs>
+              <clipPath id="b">
+                <use xlinkHref="#a" overflow="visible" />
+              </clipPath>
+              <path clip-path="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z" />
+              <path
+                clip-path="url(#b)"
+                fill="#EA4335"
+                d="M0 11l17 13 7-6.1L48 14V0H0z"
+              />
+              <path
+                clip-path="url(#b)"
+                fill="#34A853"
+                d="M0 37l30-23 7.9 1L48 0v48H0z"
+              />
+              <path
+                clip-path="url(#b)"
+                fill="#4285F4"
+                d="M48 48L17 24l-4-3 35-10z"
+              />
+            </svg>
+            <span className="mx-auto">Continue with Google</span>
+          </button>
+          <button
+            type="button"
             className="mb-2 flex h-10 items-center rounded bg-[#5865F2] px-3 text-white shadow"
             onClick={() => void handleSignIn("discord")}
           >
